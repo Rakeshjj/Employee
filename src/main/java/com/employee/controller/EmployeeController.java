ccd890647d0b9b66da242561f9d311ecb4a09af3
Rakeshjj <stand@StandPoint4>
2024-10-19 16:27:54 +0530

    Second commit

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,11 @@ public class EmployeeController {
 			return new ResponseEntity<String>(message, HttpStatus.NOT_FOUND);
 		}
 
+	}
+	
+	@GetMapping("/")
+	public String get () {
+		return "Hi Rakesh";
+		
 	}
 }
